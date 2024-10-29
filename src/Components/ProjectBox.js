b09e87d020195948211b6289a2dfaa5aed6924b7
ameen <m.ameen@canadasmm.com>
2024-10-29 19:52:16 +0300

    add project

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -5,27 +5,27 @@ import {CgFileDocument} from "react-icons/cg";
 
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
-    TindogDesc : "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub : "https://github.com/DevanshSahni/tindog",
-    TindogWebsite : "https://devanshsahni.github.io/tindog/",
+    "Shake ShakeDesc" : "Experience a whole new world of mystery boxes with Shake Shake. Whether you're on the quest for an exciting mystery box or seeking the perfect special gift, Shake Shake has changed the experience from chance to win, to win for every box ,with a simple shake! Dive into the anticipation with every mystery box you purchase as we unveil an array of surprises – from electronic devices and sleek smartphones to exclusive s coupons and gift cards from your favorite brands. With Shake Shake, satisfaction is guaranteed every time, ensuring you’ll never lose.",
+    // ShakeShakeGithub : "",
+    "Shake ShakeWebsite" : "https://www.winshakes.com/",
 
-    RogFreeDesc : "A website that shows you over seven specialized yoga postures for specific diseases or health problems. This was a group project made in a team of two for a 36-hour-long online hackathon named Hackodisha 2.0.",
-    RogFreeGithub : "https://github.com/DevanshSahni/Rog-Free",
-    RogFreeWebsite : "https://devanshsahni.github.io/Rog-Free/",
+    "Rent ChickenDesc" : "Discover the joy of raising your own chickens without the long-term commitment! At Rent-a-Chicken, we make it easy for families and individuals to enjoy the benefits of backyard hens. Our service provides everything you need for a fun and educational experience, from adorable, fully grown hens to a cozy coop and all necessary supplies.",
+    // RogFreeGithub : "",
+    "Rent ChickenWebsite" : "https://website.rentchicken.net/",
 
-    NewsletterDesc:"A newsletter signup site made using Mailchimp API where the signups can be monitored from the MailChimp account. This project was made to understand API integration, environment variables and vercel deployment.",
-    NewsletterGithub:"",
-    NewsletterWebsite:"https://newsletter-signup-teal.vercel.app/",
+    LaimonahDesc:"At Laimonah, we believe that shopping should be seamless, enjoyable, and tailored to your needs. Our e-commerce platform offers a diverse range of high-quality products, from the latest fashion trends and cutting-edge electronics to unique home goods and lifestyle essentials.",
+    // NewsletterGithub:"",
+    LaimonahWebsite:"https://website.laimonah-scc.com/",
     
-    WigglesDesc:"An innovative pet management web app enabling pet parents to create unique pet IDs, securely store and share vaccination records, and generate QR codes for pet profiles, enhancing safety.",
-    WigglesGithub:"https://github.com/DevanshSahni/Wiggles",
-    WigglesWebsite:"https://wiggles.vercel.app/",
+    "Hand StoneDesc":"Welcome to our website dedicated to artificial stone, where we merge natural beauty with modern innovation to offer high-quality products that meet all your needs. We provide a wide range of solutions made from artificial stone .",
+    // WigglesGithub:"",
+    "Hand StoneWebsite":"https://ameen014.github.io/Hand-Stone/",
   }
 
-  let show ='';
-  if(desc[projectName + 'Github']===""){
-    show="none";
-  }
+  // let show ='';
+  // if(desc[projectName + 'Github']===""){
+  //   show="none";
+  // }
     
   return (
     <div className='projectBox'> 
@@ -37,9 +37,9 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
             {desc[projectName + 'Desc']}
             <br />
 
-            <a style={{display:show}} href={desc[projectName + 'Github']} target='_blank'>
+            {/* <a style={{display:show}} href={desc[projectName + 'Github']} target='_blank'>
               <button className='projectbtn'><FaGithub/> Github</button>
-            </a>
+            </a> */}
 
             <a href={desc[projectName + 'Website']} target='_blank'>
               <button className='projectbtn'><CgFileDocument/> Demo</button>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ProjectBox from './ProjectBox';
-import NewsletterImage from '../images/NewsletterImage.png';
-import RogfreeImage from '../images/RogfreeImage.png';
-import TindogImage from '../images/TindogImage.png';
-import WigglesImage from '../images/WigglesImage.png';
+import shake from '../images/Screenshot (295).png';
+import laimonah from '../images/Screenshot (296).png';
+import stone from '../images/Screenshot (297).png';
+import rent from '../images/Screenshot (298).png';
 
 const Projects = () => {
   return (
     <div>
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
-        <ProjectBox projectPhoto={WigglesImage} projectName="Wiggles" />
-        <ProjectBox projectPhoto={NewsletterImage} projectName="Newsletter" />
-        <ProjectBox projectPhoto={RogfreeImage} projectName="RogFree" />
-        <ProjectBox projectPhoto={TindogImage} projectName="Tindog" />
+        <ProjectBox projectPhoto={shake} projectName="Shake Shake" />
+        <ProjectBox projectPhoto={laimonah} projectName="Laimonah" />
+        <ProjectBox projectPhoto={stone} projectName="Hand Stone" />
+        <ProjectBox projectPhoto={rent} projectName="Rent Chicken" />
       </div>
 
     </div>
